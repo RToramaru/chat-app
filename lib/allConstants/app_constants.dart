9d26fdf34a2e3efd11c873c08391cf355a2a0922
rafael almeida <rafael.almeida.soares2012@gmail.com>
2022-05-11 20:04:50 -0300

    criacao de constantes e widges para o projeto

---
+++ lib/allConstants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const appTitle = "Bate Papo";
+  static const loginTitle = "Página de Login";
+  static const homeTitle = "Página Princiapl";
+  static const perfilTitle = "Página de Perfil";
+  static const fotoTitle = "Foto";
+}
---
+++ lib/allConstants/firestore_constants.dart
@@ -0,0 +1,16 @@
+class FirestoreConstants {
+  static const pathUsuariosCollection = "usuarios";
+  static const pathMensagensCollection = "mensagens";
+  static const nome = "nome";
+  static const sobre = "sobre";
+  static const fotoUrl = "fotoUrl";
+  static const telefone = "telefone";
+  static const id = "id";
+  static const conversando = "conversando";
+  static const idFrom = "idFrom";
+  static const idTo = "idTo";
+  static const data = "data";
+  static const content = "content";
+  static const type = "type";
+  static const email = "email";
+}
